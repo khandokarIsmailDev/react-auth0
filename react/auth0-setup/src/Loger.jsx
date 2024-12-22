540f193d0e26bc98df7e5d8e5c3bd01923b4210a
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-12-22 17:52:29 +0600

    update auth0 config for update

--- react/auth0-setup/src/Loger.jsx
+++ react/auth0-setup/src/Loger.jsx
@@ -14,12 +14,37 @@ export default function Logger() {
     logout,
   } = useAuth0()
 
+  async function callApi() {
+    const response = await fetch('http://localhost:3000/')
+    const data = await response.json()
+    console.log(data)
+  }
+
+  async function callApiProtected() {
+    const response = await fetch('http://localhost:3000/protected')
+    const data = await response.json()
+    console.log(data)
+  }
+
   return (
     <div>
       <h1>Logger</h1>
       <button onClick={() => loginWithPopup()}>Login with Popup</button>
       <button onClick={() => logout()}>Logout</button>
       <button onClick={() => loginWithRedirect()}>Login with Redirect</button>
+
+      <div style={{ marginTop: '20px', marginBottom: '20px' }}>
+        <ul>
+          <li>
+            <button onClick={callApi}>call api route</button>
+          </li>
+          <li>
+            <button onClick={callApiProtected}>Call api protected route</button>
+          </li>
+          
+        </ul>
+      </div>
+
       <h2>User</h2>
       <p>{isAuthenticated ? 'Logged in' : 'not logged in'}</p>
       <p>{isLoading ? 'Loading...' : ''}</p>
