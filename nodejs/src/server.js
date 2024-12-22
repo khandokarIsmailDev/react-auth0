1a8efe970fb1e9ea5216b729856c10da06ed31d8
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-12-22 19:05:14 +0600

    verifyf jwt for protected route; only work form react, but not in postman; will fix it

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -1,9 +1,27 @@
+const dotenv = require('dotenv')
 const express = require('express')
 const cors = require('cors')
+const { expressjwt: jwt } = require('express-jwt');
+const jwks = require("jwks-rsa")
+const axios = require("axios")
 
+dotenv.config()
 const app = express()
 
 app.use(cors())
+
+//verify token
+const verifyJwt = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256']
+}).unless({ path: ['/'] })  //except / route ; mane "/" route chara ar kono route access korte parbe na; unless use na korle "/" aitaw protected hobe;
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World' })
